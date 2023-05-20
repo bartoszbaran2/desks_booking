1eb81749b94a632aeea173d61778fe282081b09f
Bartosz Baran <bartosz.baran192@gmail.com>
2023-05-20 21:52:00 +0200

    models for booking functionality and registration in admin panel with custom confiuguration

--- backend/booking/admin.py
+++ backend/booking/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+class ReservationAdminConfig(admin.ModelAdmin):
+    list_display = ("user", "desk", "reservation_date", "date_reserved")
+    list_editable = ("desk", "reservation_date")
+    list_filter = ("user", "desk", "reservation_date")
+    save_on_top = True
+    list_per_page = 50
+
+
+admin.site.register(models.Department)
+admin.site.register(models.Desk)
+admin.site.register(models.Reservation, ReservationAdminConfig)
--- backend/booking/models.py
+++ backend/booking/models.py
@@ -1,3 +1,26 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+
+class Department(models.Model):
+    name = models.CharField(max_length=64)
+
+    def __str__(self):
+        return self.name
+
+
+class Desk(models.Model):
+    department = models.ForeignKey("Department", on_delete=models.CASCADE, related_name="desks")
+
+    def __str__(self):
+        return self.department.name[:5] + str(self.id)
+
+
+class Reservation(models.Model):
+    user = models.ForeignKey(get_user_model(), on_delete=models.DO_NOTHING, related_name="reservations")
+    desk = models.ForeignKey("Desk", on_delete=models.CASCADE, related_name="reservations")
+    reservation_date = models.DateField(null=True, blank=True)
+    date_reserved = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"User: {self.user.username} | Date:{self.reservation_date}"
