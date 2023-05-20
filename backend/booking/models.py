8d16f02a3bf66eb921a4ac7e29195e5f9bf85c69
Bartosz Baran <bartosz.baran192@gmail.com>
2023-05-20 22:39:13 +0200

    generator of unique desk id based on department and current amount of desks

--- backend/booking/models.py
+++ backend/booking/models.py
@@ -11,9 +11,23 @@ class Department(models.Model):
 
 class Desk(models.Model):
     department = models.ForeignKey("Department", on_delete=models.CASCADE, related_name="desks")
+    desk_number = models.CharField(editable=False)
 
     def __str__(self):
-        return self.department.name[:5] + str(self.id)
+        return self.desk_number
+
+    def generate_desk_number(self):
+        """Generates a unique desk id based on the current amount of desk in given department and department name
+        :rtype: str
+        """
+        desks_in_department = Desk.objects.filter(department=self.department).count()
+        desk_number = f"{self.department.name}0{desks_in_department + 1}"
+        return desk_number
+
+    def save(self, *args, **kwargs):
+        if not self.desk_number:
+            self.desk_number = self.generate_desk_number()
+        super().save(*args, **kwargs)
 
 
 class Reservation(models.Model):
